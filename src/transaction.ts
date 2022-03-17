d3f614ee81ee2bd00f3968f123875a936a2473ed
Elmer Bulthuis <elmerbulthuis@gmail.com>
2022-03-17 21:18:22 +0100

    transaciton wip

--- src/transaction.ts
+++ src/transaction.ts
@@ -3,22 +3,33 @@ import { Promisable } from "type-fest";
 import { Access } from "./access.js";
 
 export async function withTransaction<T>(
-    pool: pg.Pool,
-    job: (context: Access) => Promisable<T>,
+    poolOrClient: pg.Pool | pg.ClientBase,
+    job: (access: Access) => Promisable<T>,
 ): Promise<T> {
-    const client = await pool.connect();
-    const context = new Access(client);
+    if (poolOrClient instanceof pg.Pool) {
+        const client = await poolOrClient.connect();
+        try {
+            const result = withTransaction(client, job);
+            client.release();
+            return result;
+        }
+        catch (error) {
+            client.release(true);
+            throw error;
+        }
 
+    }
+
+    const client = poolOrClient;
     await client.query("BEGIN TRANSACTION;");
     try {
-        const result = await job(context);
+        const access = new Access(client);
+        const result = await job(access);
         await client.query("COMMIT TRANSACTION;");
-        client.release();
         return result;
     }
     catch (err) {
         await client.query("ROLLBACK TRANSACTION;");
-        client.release(true);
         throw err;
     }
 }
